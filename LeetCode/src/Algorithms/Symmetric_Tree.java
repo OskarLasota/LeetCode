27ddcc483eabf727f541e4afb045ddbf3fe0c122
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-02-28 15:20:21 +0000

    solution added

--- LeetCode/src/Algorithms/Symmetric_Tree.java
+++ LeetCode/src/Algorithms/Symmetric_Tree.java
@@ -2,4 +2,26 @@ package Algorithms;
 
 public class Symmetric_Tree {
 
+	
+		
+	class Solution {
+		
+	
+	    public boolean isSymmetric(TreeNode root) {
+	        return isSymmetric(root, root);
+	    }
+	    
+	    
+	    public boolean isSymmetric(TreeNode node1, TreeNode node2){
+	        if((node1 == null) && (node2 == null))
+	            return true; // both are null
+	        if((node1 == null) || (node2 == null))
+	            return false; //one is null one isn't
+	        
+	        return node1.val == node2.val && isSymmetric(node1.left, node2.right) && isSymmetric(node1.right, node2.left) ;
+	    }
+	
+	}
+	    
+	
 }
