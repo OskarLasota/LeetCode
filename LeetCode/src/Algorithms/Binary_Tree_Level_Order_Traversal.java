b28e93603cc71852bbefb01b3bbc5d17dd2ebccd
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-02-28 15:23:01 +0000

    created new test

---
+++ LeetCode/src/Algorithms/Binary_Tree_Level_Order_Traversal.java
@@ -0,0 +1,17 @@
+package Algorithms;
+
+import java.util.List;
+
+public class Binary_Tree_Level_Order_Traversal {
+
+	class Solution {
+	    public List<List<Integer>> levelOrder(TreeNode root) {
+	    	
+	    	
+	    	
+			return null;
+	    }
+	}
+	
+	
+}
